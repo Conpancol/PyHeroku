5210e0acc4f554c69172f1085bd879b0548a927b
andres0sorio <osorio.af@gmail.com>
2018-09-18 09:36:16 -0500

    [Config] empty url redirects to login page

--- CPFrontend/gettingstarted/urls.py
+++ CPFrontend/gettingstarted/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include
 from django.urls import path
+from django.shortcuts import redirect
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
+    path('', lambda request: redirect('auth/login')),
     path('auth/', include('authentication.urls')),
     path('menu/', include('menu.urls')),
     path('materials/', include('materials.urls')),
